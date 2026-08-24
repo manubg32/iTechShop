89aff0ab0c4c019afea04e40c2990160091db9ab
manubg32 <manubg32@gmail.com>
2026-08-24 22:30:19 +0200

    Unit Test creation & Username refactoring
    Unit Tests creation with JUnit for Username.java class

--- user-service/src/main/java/com/itechshop/user/domain/model/Username.java
+++ user-service/src/main/java/com/itechshop/user/domain/model/Username.java
@@ -1,34 +1,52 @@
 package com.itechshop.user.domain.model;
 
-import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Username {
+    private static final Pattern PATTERN = Pattern.compile("^[a-z0-9]+$");
 
     private final String value;
 
     public Username(String value) {
-        if (value != null){
-            value = value.toLowerCase();
-            if (value.length() >= 3 && value.length() <= 20) {
-                Pattern pattern = Pattern.compile("^[a-z0-9]+$");
-                Matcher matcher = pattern.matcher(value);
-                if (matcher.matches()){
-                    this.value = value;
-                } else {
-                    throw new IllegalArgumentException("Username must contain only alphanumerical characters");
-                }
-            } else {
-                throw new IllegalArgumentException("Username must contain between 3 and 20 characters");
-            }
-        } else {
+        // Guard Clauses
+        if (value == null) {
             throw new IllegalArgumentException("Username mustn't be empty");
         }
+        value = value.toLowerCase();
+        if (value.length() < 3 || value.length() > 20) {
+            throw new IllegalArgumentException("Username must contain between 3 and 20 characters");
+        }
+        Matcher matcher = PATTERN.matcher(value);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("Username must contain only alphanumerical characters");
+        }
+        this.value = value;
+
     }
 
-    public String getValue(){
+    public String getValue() {
         return value;
     }
 
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof Username)) {
+            return false;
+        }
+
+        Username other = (Username) obj;
+
+        return this.value.equals(other.value);
+
+    }
 }
